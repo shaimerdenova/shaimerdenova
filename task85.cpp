49997525a1926b30a90a32214cf9425775f78be0
shaimerdenova <57902234+shaimerdenova@users.noreply.github.com>
2019-11-22 13:29:27 +0700

    Add files via upload

---
+++ task85.cpp
@@ -0,0 +1,192 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int n;
+cin>>n;
+for (int i=0; i<1; i++){
+        for (int j=0; j<15; j++){
+            cout<<"[";
+        }
+        for (int j=15; j<24; j++){
+            cout<<" ";
+        }
+        for (int j=24; j<39; j++){
+            cout<<"]";
+        }
+        cout<<endl;
+        }
+for (int i=1; i<3; i++){
+        for (int k=0; k<1; k++){
+            cout<<"[";}
+        for (int k=1; k<15; k++){
+            cout<< ":";}
+        for (int k=15; k<24; k++){
+            cout<< " ";}
+        for (int k=24; k<38; k++){
+            cout<<":";}
+        for (int k=38; k<39; k++){
+            cout<< "]";
+        }
+        cout<<endl;
+        }
+for (int i=3; i<4; i++){
+        for (int r=0; r<1; r++){
+            cout<<"[";}
+        for (int r=1; r<7; r++){
+            cout<< ":";}
+        for (int r=7; r<14; r++){
+            cout<< "[";}
+        for (int r=14; r<15; r++){
+            cout<<":";}
+        for (int r=15; r<24; r++){
+            cout<< " ";
+        }
+        for (int r=24; r<25; r++){
+            cout<<":";
+        }
+        for (int r=25; r<32; r++){
+            cout<<"]";
+        }
+        for (int r=32; r<38; r++){
+            cout<<":";
+        }
+        for (int r=38; r<39; r++){
+            cout<<"]";
+        }
+        cout<<endl;
+        }
+ for (int i=4; i<n-8; i++){
+        if (n<13){
+       for (int t=0; t<1; t++){
+            cout<<"[";
+        }
+        for (int t=1; t<6; t++){
+            cout<<":";
+        }
+        for (int t=6; t<7; t++){
+            cout<<"[";
+        }
+        for (int t=7; t<32; t++){
+            cout<<" ";
+        }
+        for (int t=32; t<33; t++){
+            cout<<"]";
+        }
+        for (int t=33; t<38; t++){
+            cout<<":";
+        }
+        for (int t=38; t<39; t++){
+            cout<<"]";
+        }
+        }
+        if (n>13){
+        for (int t=0; t<1; t++){
+            cout<<"[";
+        }
+        for (int t=1; t<6; t++){
+            cout<<":";
+        }
+        for (int t=6; t<7; t++){
+            cout<<"[";
+        }
+        if (i==(n-6)/2){
+cout<<"    CODE THE WORLD";
+for (int t=25; t<32; t++){
+            cout<<" ";}
+for (int t=32; t<33; t++){
+            cout<<"]";
+        }
+        for (int t=33; t<38; t++){
+            cout<<":";
+        }
+        for (int t=38; t<39; t++){
+            cout<<"]";
+        }
+}
+if (i==((n-6)/2)+1){
+cout<<"  www.itmathrepetitor.ru";
+for (int t=31; t<32; t++){
+            cout<<" ";}
+for (int t=32; t<33; t++){
+            cout<<"]";
+        }
+        for (int t=33; t<38; t++){
+            cout<<":";
+        }
+        for (int t=38; t<39; t++){
+            cout<<"]";
+        }}
+for (int t=7; t<32; t++){
+            cout<<" ";}
+for (int t=32; t<33; t++){
+            cout<<"]";
+        }
+        for (int t=33; t<38; t++){
+            cout<<":";
+        }
+        for (int t=38; t<39; t++){
+            cout<<"]";
+        }
+        }
+
+        cout<<endl;
+        }
+for (int i=n+5; i<n+6; i++){
+        for (int s=0; s<1; s++){
+            cout<<"[";}
+        for (int s=1; s<7; s++){
+            cout<< ":";}
+        for (int s=7; s<14; s++){
+            cout<< "[";}
+        for (int s=14; s<15; s++){
+            cout<<":";}
+        for (int s=15; s<24; s++){
+            cout<< " ";
+        }
+        for (int s=24; s<25; s++){
+            cout<<":";
+        }
+        for (int s=25; s<32; s++){
+            cout<<"]";
+        }
+        for (int s=32; s<38; s++){
+            cout<<":";
+        }
+        for (int s=38; s<39; s++){
+            cout<<"]";
+        }
+        cout<<endl;
+        }
+for (int i=n+6; i<n+8; i++){
+        for (int k=0; k<1; k++){
+            cout<<"[";}
+        for (int k=1; k<15; k++){
+            cout<< ":";}
+        for (int k=15; k<24; k++){
+            cout<< " ";}
+        for (int k=24; k<38; k++){
+            cout<<":";}
+        for (int k=38; k<39; k++){
+            cout<< "]";
+        }
+        cout<<endl;
+        }
+for (int i=n+8; i<n+9; i++){
+        for (int j=0; j<15; j++){
+            cout<<"[";
+        }
+        for (int j=15; j<24; j++){
+            cout<<" ";
+        }
+        for (int j=24; j<39; j++){
+            cout<<"]";
+        }
+        cout<<endl;
+        }
+         return 0;
+}
+
+
